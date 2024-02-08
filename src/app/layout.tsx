34f96e9825efcd4e9face015ee31a53a94103ac9
Luiz Teles <luizteles934@gmail.com>
2024-02-07 21:42:12 -0300

    chore: Importado o header da página no layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
-import type { Metadata } from 'next';
-import { Roboto } from 'next/font/google';
-import '@/styles/globals.css';
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "@/styles/globals.css";
+import Header from "@/components/Header/Header";
 
-const roboto = Roboto({ subsets: ['latin'], weight: ['400'] });
+const roboto = Roboto({ subsets: ["latin"], weight: ["400"] });
 
 export const metadata: Metadata = {
-  title: 'betterbox',
-  description: '',
+  title: "betterbox",
+  description: "",
 };
 
 export default function RootLayout({
@@ -17,7 +18,8 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body className={roboto.className}>
-        <main>{children}</main>
+        <Header />
+        <main className="pt-20">{children}</main>
       </body>
     </html>
   );
