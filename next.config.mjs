221d7e77b44dbd1d5f223dd7dd8469572e7899be
Luiz Teles <luizteles934@gmail.com>
2024-02-09 02:02:43 -0300

    chore: Permissão para a aplicação usar imagens do TMDB

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        port: "",
+        pathname: "/t/p/original/**",
+      },
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        port: "",
+        pathname: "/t/p/w500/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
