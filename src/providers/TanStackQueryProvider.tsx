f20fd73fd06d01dc9d845ccf9a1787b31e8a6de4
Luiz Teles <luizteles934@gmail.com>
2024-02-09 23:29:54 -0300

    feat: Provider do ReactQuery

---
+++ src/providers/TanStackQueryProvider.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+const TanStackQueryProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+export default TanStackQueryProvider;
