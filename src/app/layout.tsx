38d98dbbbcd414f29320ef43b841ad25c96f7b89
Luiz Teles <luizteles934@gmail.com>
2024-02-09 23:37:04 -0300

    chore: Adicionado o provider do ReactQuery no layout principal da aplicação

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Roboto } from "next/font/google";
 import "@/styles/globals.css";
 import Header from "@/components/Header/Header";
 
+import TanStackQueryProvider from "@/providers/TanStackQueryProvider";
+
 const roboto = Roboto({ subsets: ["latin"], weight: ["400"] });
 
 export const metadata: Metadata = {
@@ -19,7 +21,9 @@ export default function RootLayout({
     <html lang="pt-br">
       <body className={roboto.className}>
         <Header />
-        <main className="pt-20">{children}</main>
+        <main className="mx-auto mt-20 max-w-5xl px-5 py-10">
+          <TanStackQueryProvider>{children}</TanStackQueryProvider>
+        </main>
       </body>
     </html>
   );
