9b3a1976ca9c25554af41d8fbc1b1799c5df6f19
Luiz Teles <luizteles934@gmail.com>
2024-02-10 21:31:26 -0300

    chore: Importação do provedor de temas no layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "@/styles/globals.css";
 import Header from "@/components/Header/Header";
 
 import TanStackQueryProvider from "@/providers/TanStackQueryProvider";
+import { ThemeProvider } from "@/providers/ThemeProvider";
 
 const roboto = Roboto({ subsets: ["latin"], weight: ["400"] });
 
@@ -20,10 +21,12 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body className={roboto.className}>
-        <Header />
-        <main className="mx-auto mt-20 max-w-5xl px-5 py-10">
-          <TanStackQueryProvider>{children}</TanStackQueryProvider>
-        </main>
+        <ThemeProvider attribute="class" defaultTheme="system">
+          <Header />
+          <main className="mx-auto mt-20 max-w-5xl px-5 py-10">
+            <TanStackQueryProvider>{children}</TanStackQueryProvider>
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   );
