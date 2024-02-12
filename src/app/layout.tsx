02da4d1aac46cc79526a8dfd4e7686e288a5aa68
Luiz Teles <luizteles934@gmail.com>
2024-02-12 20:19:16 -0300

    feat: Implementado os modals no layout principal da aplicação

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,13 @@
-import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "@/styles/globals.css";
+
 import Header from "@/components/Header/Header";
+import AuthModal from "@/components/AuthModal/AuthModal";
 
 import TanStackQueryProvider from "@/providers/TanStackQueryProvider";
-import { ThemeProvider } from "@/providers/ThemeProvider";
+import ThemeProvider from "@/providers/ThemeProvider";
 
 const roboto = Roboto({ subsets: ["latin"], weight: ["400"] });
-
-export const metadata: Metadata = {
-  title: "betterbox",
-  description: "",
-};
 
 export default function RootLayout({
   children,
@@ -23,6 +19,7 @@ export default function RootLayout({
       <body className={roboto.className}>
         <ThemeProvider attribute="class" defaultTheme="system">
           <Header />
+          <AuthModal />
           <main className="mx-auto mt-20 max-w-5xl px-5 py-10">
             <TanStackQueryProvider>{children}</TanStackQueryProvider>
           </main>
