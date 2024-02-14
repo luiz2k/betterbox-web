bf94e6f8c976fe95c16c44eb6ac2437d895ea5f4
Luiz Teles <luizteles934@gmail.com>
2024-02-14 11:32:11 -0300

    chore: Importado o menu drop-down no header e adicionado alterar tema dentro da sidebar em telas menores

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,8 +2,11 @@
 
 import Button from "../Button/Button";
 import NavLink from "../NavLink/NavLink";
+import DropDown from "../DropDown";
 
 import { useRouter } from "next/navigation";
+import { useTheme } from "next-themes";
+import { useEffect } from "react";
 
 import { useHeaderStore } from "@/stores/HeaderStore";
 
@@ -19,9 +22,6 @@ import {
   SunMoon,
   X,
 } from "lucide-react";
-
-import { useTheme } from "next-themes";
-import { useEffect } from "react";
 
 const Header = () => {
   const {
@@ -89,119 +89,138 @@ const Header = () => {
   }, [handleThemMenu, themeMenu]);
 
   return (
-    <header className="fixed inset-x-0 top-0 z-10 flex h-20 items-center justify-between gap-5 border-b bg-color-2 p-5 text-base dark:bg-color-4">
-      <div className="font-bold">LOGO</div>
+    <header className="fixed inset-x-0 top-0 z-10 bg-color-2 dark:bg-color-4">
+      <div className="flex h-20 items-center justify-between gap-5 border-b bg-color-2 p-5 text-base dark:bg-color-4">
+        <div className="font-bold">LOGO</div>
 
-      <div className="flex gap-5">
-        <nav
-          data-nav={navBar}
-          className="absolute -left-60 top-20 h-screen w-60 border-r bg-color-2 p-5 duration-200 data-[nav=true]:left-0 dark:bg-color-4 md:sticky md:h-auto md:w-fit md:border-none md:bg-transparent md:p-0 dark:md:bg-transparent"
-        >
-          <ul className=" flex flex-col gap-5 uppercase md:flex-row">
-            <li>
-              <NavLink href="/" leftIcon={<Home />}>
-                Início
-              </NavLink>
-            </li>
-            <li>
-              <NavLink href="/filmes/emalta" leftIcon={<Flame />}>
-                Em Alta
-              </NavLink>
-            </li>
-            <li>
-              <NavLink href="/filmes/topfilmes" leftIcon={<Popcorn />}>
-                Top filmes
-              </NavLink>
-            </li>
-          </ul>
-
-          <span
-            className="absolute right-1 top-1 cursor-pointer rounded-full md:hidden"
-            onClick={handleNavBar}
+        <div className="flex gap-5">
+          <div
+            data-sidebar={navBar}
+            className="absolute -left-60 top-0 flex h-svh w-60 flex-col justify-between border-r bg-color-2 p-5 duration-200 data-[sidebar=true]:left-0 dark:bg-color-4 md:static md:h-auto md:w-fit md:border-none md:bg-transparent md:p-0 dark:md:bg-transparent"
           >
-            <X />
-          </span>
-        </nav>
+            <nav>
+              <ul className=" flex flex-col gap-5 uppercase md:flex-row">
+                <li>
+                  <NavLink href="/" leftIcon={<Home />}>
+                    Início
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink href="/filmes/emalta" leftIcon={<Flame />}>
+                    Em Alta
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink href="/filmes/topfilmes" leftIcon={<Popcorn />}>
+                    Top filmes
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
 
-        <form
-          data-search={searchBar}
-          onSubmit={handleSearch}
-          className="absolute inset-x-0 -top-20 z-10 flex h-20 items-center justify-center border-b bg-color-2 p-5 px-10 duration-200 data-[search=true]:-top-0 dark:bg-color-4 md:sticky md:h-fit md:border-none md:bg-transparent md:p-0 dark:md:bg-transparent"
-        >
-          <div className="relative w-full">
-            <input
-              type="text"
-              name="search"
-              className="w-full rounded-full border bg-color-2 py-1 pl-2 pr-8 text-color-4 shadow-inner md:p-0 md:pl-2 md:pr-7"
-            />
+            <menu className="space-y-2 md:hidden">
+              <li className="rounded-md bg-color-2 dark:bg-color-4">
+                <Button
+                  leftIcon={<Sun />}
+                  width="full"
+                  onClick={() => setTheme("light")}
+                >
+                  Claro
+                </Button>
+              </li>
+              <li className="rounded-md bg-color-2 dark:bg-color-4">
+                <Button
+                  leftIcon={<MoonStar />}
+                  width="full"
+                  onClick={() => setTheme("dark")}
+                >
+                  Escuro
+                </Button>
+              </li>
+              <li className="rounded-md bg-color-2 dark:bg-color-4">
+                <Button
+                  leftIcon={<Computer />}
+                  width="full"
+                  onClick={() => setTheme("system")}
+                >
+                  Sistema
+                </Button>
+              </li>
+            </menu>
 
-            <button
-              type="submit"
-              className="absolute right-1.5 top-[0.32rem] rounded-full md:top-[0.20rem]"
+            <span
+              className="absolute right-1 top-1 cursor-pointer rounded-full md:hidden"
+              onClick={handleNavBar}
             >
-              <Search className="text-color-4 md:size-5" />
-            </button>
+              <X />
+            </span>
           </div>
 
-          <span
-            className="absolute right-1 top-1 cursor-pointer md:hidden"
-            onClick={handleSearchBar}
+          <form
+            data-search={searchBar}
+            onSubmit={handleSearch}
+            className="absolute inset-x-0 top-0 -z-10 flex h-20 items-center justify-center border-b bg-color-2 p-5 px-10 duration-200 data-[search=true]:top-20 dark:bg-color-4 md:static md:z-0 md:h-fit md:border-none md:bg-transparent md:p-0 dark:md:bg-transparent"
           >
-            <X />
-          </span>
-        </form>
-      </div>
+            <div className="relative w-full">
+              <input
+                type="text"
+                name="search"
+                className="w-full rounded-full border bg-color-2 py-1 pl-2 pr-8 text-color-4 shadow-inner md:p-0 md:pl-2 md:pr-7"
+              />
 
-      <div className="flex gap-2">
-        <span className="md:hidden">
-          <Button leftIcon={<Menu />} onClick={handleNavBar} />
-        </span>
+              <button
+                type="submit"
+                className="absolute right-1.5 top-[0.32rem] rounded-full md:top-[0.20rem]"
+              >
+                <Search className="text-color-4 md:size-5" />
+              </button>
+            </div>
 
-        <span className="md:hidden">
-          <Button leftIcon={<Search />} onClick={handleSearchBar} />
-        </span>
+            <span
+              className="absolute right-1 top-1 cursor-pointer md:hidden"
+              onClick={handleSearchBar}
+            >
+              <X />
+            </span>
+          </form>
+        </div>
 
-        <span className="relative z-0">
-          <Button leftIcon={<SunMoon />} onClick={handleThemMenu} />
+        <div className="flex gap-2">
+          <div className="md:hidden">
+            <Button leftIcon={<Search />} onClick={handleSearchBar} />
+          </div>
 
-          <menu
-            data-animation={themeMenu}
-            className="invisible absolute right-0 top-10 translate-y-2 space-y-0.5 rounded bg-color-4 p-1 text-color-2 opacity-0 duration-200 data-[animation=true]:visible data-[animation=true]:translate-y-0 data-[animation=true]:opacity-100 dark:bg-color-2 dark:text-color-4"
+          <div className="md:hidden">
+            <Button leftIcon={<Menu />} onClick={handleNavBar} />
+          </div>
+
+          <DropDown.Root
+            leftIcon={<SunMoon />}
+            className="hidden md:block"
+            onClick={handleThemMenu}
           >
-            <li className="rounded-md bg-color-2 dark:bg-color-4">
-              <Button
+            <DropDown.Menu animation={themeMenu}>
+              <DropDown.Option
                 leftIcon={<Sun />}
-                cursor="default"
-                width="full"
                 onClick={() => setTheme("light")}
               >
                 Claro
-              </Button>
-            </li>
-
-            <li className="rounded-md bg-color-2 dark:bg-color-4">
-              <Button
+              </DropDown.Option>
+              <DropDown.Option
                 leftIcon={<MoonStar />}
-                cursor="default"
-                width="full"
                 onClick={() => setTheme("dark")}
               >
                 Escuro
-              </Button>
-            </li>
-
-            <li className="rounded-md bg-color-2 dark:bg-color-4">
-              <Button
+              </DropDown.Option>
+              <DropDown.Option
                 leftIcon={<Computer />}
-                cursor="default"
-                width="full"
                 onClick={() => setTheme("system")}
               >
                 Sistema
-              </Button>
-            </li>
-          </menu>
-        </span>
+              </DropDown.Option>
+            </DropDown.Menu>
+          </DropDown.Root>
+        </div>
       </div>
     </header>
   );
