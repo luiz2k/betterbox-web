1d50f3636bb2421ee0bfb1bb60dcfdc550cb1f64
Luiz Teles <luizteles934@gmail.com>
2024-02-14 16:43:30 -0300

    feat: Implementado um menu para abrir e fechar o modal de registro e login

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,17 +9,21 @@ import { useTheme } from "next-themes";
 import { useEffect } from "react";
 
 import { useHeaderStore } from "@/stores/HeaderStore";
+import { useAuthModalStore } from "@/stores/AuthModalStore";
 
 import {
   Computer,
   Flame,
   Home,
+  KeyRound,
+  LogIn,
   Menu,
   MoonStar,
   Popcorn,
   Search,
   Sun,
   SunMoon,
+  User,
   X,
 } from "lucide-react";
 
@@ -32,6 +36,8 @@ const Header = () => {
     themeMenu,
     handleThemMenu,
   } = useHeaderStore();
+
+  const { handleSignInModal, handleSignUpModal } = useAuthModalStore();
 
   const { setTheme } = useTheme();
 
@@ -96,7 +102,7 @@ const Header = () => {
         <div className="flex gap-5">
           <div
             data-sidebar={navBar}
-            className="absolute -left-60 top-0 flex h-svh w-60 flex-col justify-between border-r bg-color-2 p-5 duration-200 data-[sidebar=true]:left-0 dark:bg-color-4 md:static md:h-auto md:w-fit md:border-none md:bg-transparent md:p-0 dark:md:bg-transparent"
+            className="absolute -left-60 top-0 z-10 flex h-svh w-60 flex-col justify-between border-r bg-color-2 p-5 duration-200 data-[sidebar=true]:left-0 dark:bg-color-4 md:static md:h-auto md:w-fit md:border-none md:bg-transparent md:p-0 dark:md:bg-transparent"
           >
             <nav>
               <ul className=" flex flex-col gap-5 uppercase md:flex-row">
@@ -194,31 +200,45 @@ const Header = () => {
             <Button leftIcon={<Menu />} onClick={handleNavBar} />
           </div>
 
-          <DropDown.Root
-            leftIcon={<SunMoon />}
-            className="hidden md:block"
-            onClick={handleThemMenu}
-          >
-            <DropDown.Menu animation={themeMenu}>
-              <DropDown.Option
-                leftIcon={<Sun />}
-                onClick={() => setTheme("light")}
-              >
-                Claro
-              </DropDown.Option>
-              <DropDown.Option
-                leftIcon={<MoonStar />}
-                onClick={() => setTheme("dark")}
-              >
-                Escuro
-              </DropDown.Option>
-              <DropDown.Option
-                leftIcon={<Computer />}
-                onClick={() => setTheme("system")}
-              >
-                Sistema
-              </DropDown.Option>
-            </DropDown.Menu>
+          <DropDown.Root leftIcon={<SunMoon />} className="hidden md:block">
+            <DropDown.Option
+              leftIcon={<Sun />}
+              cursor="default"
+              onClick={() => setTheme("light")}
+            >
+              Claro
+            </DropDown.Option>
+            <DropDown.Option
+              leftIcon={<MoonStar />}
+              cursor="default"
+              onClick={() => setTheme("dark")}
+            >
+              Escuro
+            </DropDown.Option>
+            <DropDown.Option
+              leftIcon={<Computer />}
+              cursor="default"
+              onClick={() => setTheme("system")}
+            >
+              Sistema
+            </DropDown.Option>
+          </DropDown.Root>
+
+          <DropDown.Root leftIcon={<User />}>
+            <DropDown.Option
+              leftIcon={<LogIn />}
+              cursor="default"
+              onClick={handleSignInModal}
+            >
+              Entrar
+            </DropDown.Option>
+            <DropDown.Option
+              leftIcon={<KeyRound />}
+              cursor="default"
+              onClick={handleSignUpModal}
+            >
+              Registrar
+            </DropDown.Option>
           </DropDown.Root>
         </div>
       </div>
