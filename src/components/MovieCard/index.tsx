9c17eb2584946afbecb0bb6cd168041eeaa05cec
Luiz Teles <luizteles934@gmail.com>
2024-02-22 15:38:10 -0300

    feat: Exportar todos as partes do componente MovieCard

---
+++ src/components/MovieCard/index.tsx
@@ -0,0 +1,19 @@
+import MovieCardImage from "./MovieCardImage";
+import MovieCardInfos from "./MovieCardInfos";
+import MovieCardInfosTitle from "./MovieCardInfosTitle";
+import MovieCardVoteAverageAndReleaseDate from "./MovieCardVoteAverageAndReleaseDate";
+import MovieCardRoot from "./MovieCardRoot";
+import MovieCardVoteAverage from "./MovieCardVoteAverage";
+import MovieCardReleaseDate from "./MovieCardReleaseDate";
+
+const MovieCard = {
+  Root: MovieCardRoot,
+  Image: MovieCardImage,
+  Infos: MovieCardInfos,
+  Title: MovieCardInfosTitle,
+  VoteAverageAndReleaseDate: MovieCardVoteAverageAndReleaseDate,
+  VoteAverage: MovieCardVoteAverage,
+  ReleaseDate: MovieCardReleaseDate,
+};
+
+export default MovieCard;
