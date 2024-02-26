6e3c32f637a87b783a9479418d216b9b3dc96507
Luiz Teles <luizteles934@gmail.com>
2024-02-26 14:19:50 -0300

    chore: Adicicionar tipagem para o id do usuário na sessão de autenticação

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -4,6 +4,7 @@ import { JWT } from "next-auth/jwt";
 declare module "next-auth" {
   interface Session {
     user: {
+      id: number;
       username: string;
       picture: string | null;
       bio: string | null;
