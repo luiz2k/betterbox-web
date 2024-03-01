b3f1da456ae426c67e8454fcdd515a1b3a8bf30e
Luiz Teles <luizteles934@gmail.com>
2024-03-01 16:12:12 -0300

    feat: Implementar carrossel no perfil do usuário

--- src/app/perfil/page.tsx
+++ src/app/perfil/page.tsx
@@ -3,11 +3,8 @@ import options from "../api/auth/[...nextauth]/options";
 import { Metadata } from "next";
 import { getAllMoviesListedByUser } from "@/services/Batterbox/Batterbox";
 
-import MovieCard from "@/components/MovieCard/index";
-import Link from "next/link";
-import Button from "@/components/Button/Button";
-
 import { GetAllMovies } from "./page.d";
+import Carousel from "@/components/Carousel/Carousel";
 
 export const metadata: Metadata = { title: "betterbox - Perfil" };
 
@@ -37,91 +34,49 @@ export default async function Profile() {
         )}
       </header>
 
-      <section className="space-y-2">
-        <header>
-          <h2 className="text-lg font-bold uppercase">Filmes assistidos</h2>
-          <p className="text-color-3">Filmes que você assistiu</p>
-        </header>
+      {watchedMovies.data?.length ? (
+        <Carousel
+          data={watchedMovies.data}
+          title="Filmes assistidos"
+          subtitle="Filmes que você assistiu"
+          href="/perfil/assistidos"
+        />
+      ) : (
+        <section>
+          <header>
+            <h2 className="text-lg font-bold uppercase">Filmes favoritos</h2>
+            <p className="text-color-3">Filmes que você assistiu</p>
+          </header>
 
-        {watchedMovies.data?.length ? (
-          <>
-            <div className="grid grid-cols-[repeat(5,148.8px)] gap-5 overflow-x-auto rounded bg-color-3/5 p-5">
-              {watchedMovies.data?.slice(0, 5).map((movie) => (
-                <MovieCard.Root
-                  title={movie.title}
-                  id={movie.id}
-                  key={movie.id}
-                >
-                  <MovieCard.Image
-                    posterPath={movie.posterPath}
-                    alt={movie.title}
-                  />
-                  <MovieCard.Infos>
-                    <MovieCard.Title title={movie.title} />
-                  </MovieCard.Infos>
-                </MovieCard.Root>
-              ))}
-            </div>
-
-            <div className="flex justify-end">
-              <Link href={"/perfil/assistidos"}>
-                <Button theme="grayFill" textColor="white" uppercase>
-                  Ver mais
-                </Button>
-              </Link>
-            </div>
-          </>
-        ) : (
           <div className="grid justify-center gap-5 overflow-x-auto rounded bg-color-3/5 p-5">
             <p className="uppercase text-color-3">
               Você não possui nenhum filme na lista de assistidos.
             </p>
           </div>
-        )}
-      </section>
+        </section>
+      )}
 
-      <section className="space-y-2">
-        <header>
-          <h2 className="text-lg font-bold uppercase">Filmes favoritos</h2>
-          <p className="text-color-3">Filmes que você favoritou</p>
-        </header>
+      {favoriteMovies.data?.length ? (
+        <Carousel
+          data={favoriteMovies.data}
+          title="Filmes assistidos"
+          subtitle="Filmes que você assistiu"
+          href="/perfil/favoritos"
+        />
+      ) : (
+        <section>
+          <header>
+            <h2 className="text-lg font-bold uppercase">Filmes favoritos</h2>
+            <p className="text-color-3">Filmes que você favoritou</p>
+          </header>
 
-        {favoriteMovies.data?.length ? (
-          <>
-            <div className="grid grid-cols-[repeat(5,148.8px)] gap-5 overflow-x-auto rounded bg-color-3/5 p-5">
-              {favoriteMovies.data?.slice(0, 5).map((movie) => (
-                <MovieCard.Root
-                  title={movie.title}
-                  id={movie.id}
-                  key={movie.id}
-                >
-                  <MovieCard.Image
-                    posterPath={movie.posterPath}
-                    alt={movie.title}
-                  />
-                  <MovieCard.Infos>
-                    <MovieCard.Title title={movie.title} />
-                  </MovieCard.Infos>
-                </MovieCard.Root>
-              ))}
-            </div>
-
-            <div className="flex justify-end">
-              <Link href={"/perfil/favoritos"}>
-                <Button theme="grayFill" textColor="white" uppercase>
-                  Ver mais
-                </Button>
-              </Link>
-            </div>
-          </>
-        ) : (
           <div className="grid justify-center gap-5 overflow-x-auto rounded bg-color-3/5 p-5">
             <p className="uppercase text-color-3">
-              Você não possui nenhum filme favoritado.
+              Você não possui nenhum filme na lista de favoritos.
             </p>
           </div>
-        )}
-      </section>
+        </section>
+      )}
     </section>
   );
 }
