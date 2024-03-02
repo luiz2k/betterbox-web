f104dfa0aa5481a7df26ac6cb9b8500dfce3e522
Luiz Teles <luizteles934@gmail.com>
2024-03-02 11:57:10 -0300

    style: Reduzir tamanho do loader

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import { Loader } from "lucide-react";
 export default function Loading() {
   return (
     <div className="flex h-[calc(100vh-160px)] items-center justify-center">
-      <Loader className="size-20 animate-spin" />
+      <Loader className="size-12 animate-spin" />
     </div>
   );
 }
