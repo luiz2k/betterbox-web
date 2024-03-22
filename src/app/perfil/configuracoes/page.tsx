f9b148160ee07666b3ad757343407fbbd0a3ee77
Luiz Teles <luizteles934@gmail.com>
2024-03-22 12:35:58 -0300

    feat: Importar o componente de lida com a foto de perfil do usuário na página de configurações

--- src/app/perfil/configuracoes/page.tsx
+++ src/app/perfil/configuracoes/page.tsx
@@ -1,6 +1,7 @@
 import options from "@/app/api/auth/[...nextauth]/options";
 import ChangeEmail from "@/components/SettingsForms/ChangeEmail/ChangeEmail";
 import ChangePassword from "@/components/SettingsForms/ChangePassword/ChangePassword";
+import ChangePicture from "@/components/SettingsForms/ChangePicture/ChangePicture";
 import ChangeUsername from "@/components/SettingsForms/ChangeUsername/ChangeUsername";
 import { Metadata } from "next";
 import { Session, getServerSession } from "next-auth";
@@ -22,6 +23,10 @@ export default async function Settings() {
           </p>
         )}
       </header>
+
+      <ChangePicture apiURL={apiBaseURL} session={session} />
+
+      <hr className="m-auto max-w-xl" />
 
       <ChangeUsername apiURL={apiBaseURL} accessToken={session?.accessToken} />
 
