9879705b9d79836482497ec4cdf9029723539011
Luiz Teles <luizteles934@gmail.com>
2024-03-23 17:11:06 -0300

    fix: Corrigir o aviso de altura e larguda da logo

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -4,7 +4,14 @@ import Link from "next/link";
 const Logo = () => {
   return (
     <Link href="/">
-      <Image src={"/logo.png"} width={50} height={50} alt={"Logo"} />
+      <Image
+        src={"/logo.png"}
+        width={50}
+        height={46.88}
+        priority
+        alt={"Logo"}
+        className="w-12"
+      />
     </Link>
   );
 };
